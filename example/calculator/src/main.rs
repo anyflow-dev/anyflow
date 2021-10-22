6721f1cfcfb46d7dc53c087fb968c7ee2941952b
salamer <salamer_gaga@163.com>
2021-10-22 17:06:05 +0800

    chore: update

--- example/calculator/src/main.rs
+++ example/calculator/src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 use std::sync::Arc;
 use serde_json::{Result, Value};
 use serde::{Deserialize, Serialize};
+use pprof;
+use std::fs::File;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Val {
@@ -13,26 +15,37 @@ struct Val {
 
 fn calc<'a, E: Send + Sync>(
     _graph_args: &'a Arc<E>,
-    _input: Arc<anyflow::dag::NodeResult>,
+    input: Arc<anyflow::dag::NodeResult>,
     params: Box<RawValue>,
 ) -> anyflow::dag::NodeResult {
-    println!("data");
     let p: Val = serde_json::from_str(params.get()).unwrap();
-    println!("params {:?}", p);
-    let t = anyflow::dag::NodeResult::new();
-    t.set("xxx", &anyflow::dag::DAGConfig::default());
-    let _c = t.get::<anyflow::dag::DAGConfig>("xxx");
-    t
+    
+
+    let val = match input.get::<i32>("res") {
+        Some(val) => val,
+        None => 0,
+    };
+    println!("params {:?} {:?}", p, val);
+    anyflow::dag::NodeResult::new().set("res", &(val + p.val))
 }
 
 #[tokio::main]
 async fn main() {
+    let guard = pprof::ProfilerGuard::new(100).unwrap();
+
     let mut dag = anyflow::dag::Flow::<i32, i32>::new();
     let data = fs::read_to_string("dag.json").expect("Unable to read file");
     println!("{:?}", dag.init(&data));
     dag.register("calc", Arc::new(calc));
-    let my_dag = dag.make_flow(Arc::new(1));
-    println!("{:?}", my_dag.await);
-    // let mut rt = tokio::runtime::Runtime::new().unwrap();
-    // let result = rt.block_on(my_dag);
+    for i in 0..1000 {
+        let my_dag = dag.make_flow(Arc::new(1));
+        println!("{:?}", my_dag.await[0].get::<i32>("res"));
+    }
+
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("flamegraph.svg").unwrap();
+        let mut options = pprof::flamegraph::Options::default();
+        options.image_width = Some(2500);
+        report.flamegraph_with_options(file, &mut options).unwrap();
+    };
 }
