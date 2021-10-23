d526275a9bb821cbf499834d748aa788420ce154
salamer <salamer_gaga@163.com>
2021-10-23 14:56:22 +0800

    chore: update

--- example/calculator/src/main.rs
+++ example/calculator/src/main.rs
@@ -1,51 +1,68 @@
-
-
-use serde_json::value::RawValue;
-use std::fs;
-use std::sync::Arc;
-use serde_json::{Result, Value};
-use serde::{Deserialize, Serialize};
 use pprof;
+use serde::{Deserialize, Serialize};
+use serde_json::value::RawValue;
+use serde_json::{Result, Value};
+use smol::{io, net, prelude::*, Unblock};
+use std::fs;
 use std::fs::File;
+use std::sync::Arc;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Val {
-    val: i32
+    val: i32,
 }
 
 fn calc<'a, E: Send + Sync>(
     _graph_args: &'a Arc<E>,
-    input: Arc<anyflow::dag::NodeResult>,
+    input: Arc<anyflow::dag::FlowResult>,
     params: Box<RawValue>,
-) -> anyflow::dag::NodeResult {
+) -> anyflow::dag::FlowResult {
     let p: Val = serde_json::from_str(params.get()).unwrap();
-    
 
-    let val = match input.get::<i32>("res") {
-        Some(val) => val,
-        None => 0,
+    let val: i32 = match input.get::<i32>("res") {
+        Ok(val) => val.clone(),
+        Err(e) => 0,
     };
     // println!("params {:?} {:?}", p, val);
-    anyflow::dag::NodeResult::new().set("res", &(val + p.val))
+    let mut r = anyflow::dag::FlowResult::new();
+    r.set("res", val + p.val);
+    r
 }
 
-#[tokio::main]
-async fn main() {
-    // let guard = pprof::ProfilerGuard::new(100).unwrap();
+fn smol_main() {
+    let mut dag = anyflow::dag::Flow::<i32, i32>::new();
+    let data = fs::read_to_string("dag.json").expect("Unable to read file");
+    println!("{:?}", dag.init(&data));
+    dag.register("calc", Arc::new(calc));
+    for i in 0..100000 {
+        let my_dag = dag.make_flow(Arc::new(1));
+        // println!("{:?}", my_dag.await[0].get::<i32>("res"));
+        smol::block_on(my_dag);
+    }
+}
+
+fn tokio_main() {
+    let guard = pprof::ProfilerGuard::new(100).unwrap();
 
     let mut dag = anyflow::dag::Flow::<i32, i32>::new();
     let data = fs::read_to_string("dag.json").expect("Unable to read file");
     println!("{:?}", dag.init(&data));
     dag.register("calc", Arc::new(calc));
-    for i in 0..100000000 {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    for i in 0..100000 {
         let my_dag = dag.make_flow(Arc::new(1));
         // println!("{:?}", my_dag.await[0].get::<i32>("res"));
+        rt.block_on(my_dag);
     }
 
-    // if let Ok(report) = guard.report().build() {
-    //     let file = File::create("flamegraph.svg").unwrap();
-    //     let mut options = pprof::flamegraph::Options::default();
-    //     options.image_width = Some(1800);
-    //     report.flamegraph_with_options(file, &mut options).unwrap();
-    // };
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("flamegraph.svg").unwrap();
+        let mut options = pprof::flamegraph::Options::default();
+        options.image_width = Some(1800);
+        report.flamegraph_with_options(file, &mut options).unwrap();
+    };
+}
+
+fn main() {
+    tokio_main();
 }
